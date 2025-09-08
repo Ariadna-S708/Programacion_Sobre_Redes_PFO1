227ef82aec58e212a41b2e252d3c704d471ec1b1
Ariadna-S708 <ariadnacursos518@gmail.com>
2025-09-08 01:34:53 -0300

    Agregada la visualizacion de los mensajes guardados en la base de datos

--- cliente.py
+++ cliente.py
@@ -24,7 +24,7 @@ def enviar_mensaje(cliente_socket: socket.socket, mensaje: str) -> None:
         mensaje = input("Escribe un mensaje (o 'éxito' para salir): ")
         
         if mensaje.lower() == 'éxito':
-            print("Cerrando conexión...")
+            print("\nCerrando conexión...\n")
             break
         
         # Enviar el mensaje al servidor
@@ -35,8 +35,42 @@ def enviar_mensaje(cliente_socket: socket.socket, mensaje: str) -> None:
         print(f"[SERVIDOR] {respuesta}")
     
     cliente_socket.close()
-    
-    
+    ver_mensajes_guardados()
+
+
+# Mostrar mensajes guardados en la base de datos
+
+def ver_mensajes_guardados():
+
+    ver = input("¿Quiere ver los mensajes guardados? ('si' para ver): ")
+
+    if ver.lower() == "si":
+        # Conectar a la base de datos
+        conn = sqlite3.connect("mensajes.db")
+        cursor = conn.cursor()
+
+        # Consultar los mensajes guardados
+        cursor.execute("SELECT * FROM mensajes")
+        mensajes = cursor.fetchall()
+
+        # Mostrar los mensajes
+        encabezado = f"| {'ID':<5} | {'Contenido':<30} | {'Fecha Envío':<20} | {'IP Cliente':<15} |"
+        print("-" * len(encabezado))  # Línea superior
+        print(encabezado)
+        print("-" * len(encabezado))  # Línea separadora
+        
+        # Mostrar cada mensaje en formato de tabla
+        for mensaje in mensajes:
+            print(f"| {mensaje[0]:<5} | {mensaje[1]:<30} | {mensaje[2]:<20} | {mensaje[3]:<15} |")
+        
+        print("-" * len(encabezado))  # Línea inferior
+
+        # Cerrar conexión a la base de datos
+        conn.close()
+    else:
+        print("No se mostraran los mensajes guardados. Adiós!\n")
+
+
 # Configuración del cliente
 
 if __name__ == '__main__':
